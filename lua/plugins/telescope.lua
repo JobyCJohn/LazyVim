beb66305095dbda73f254e74fa2b5c0ac075cfc6
Joby C John <11912680+JobyCJohn@users.noreply.github.com>
2024-07-28 16:32:21 +0530

    following lazyvim folder structure

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,55 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	opts = {
-		defaults = {
-			layout_config = { prompt_position = "top" },
-			sorting_strategy = "ascending",
-			wrap_results = true,
-			path_display = { "smart" },
-			mappings = {
-				i = {
-					["<Esc>"] = require("telescope.actions").close,
-				},
-			},
+	cmd = "Telescope",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
 		},
 	},
-	keys = {
-		{ "<leader><Space>", false },
-	},
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				prompt_prefix = " ",
+				selection_caret = " ",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				wrap_results = true,
+				path_display = { "smart" },
+				file_ignore_patterns = {
+					"node_modules",
+					".git/*",
+					"%.zip",
+					"%.exe",
+					"%.dll",
+					"%.pdb",
+				},
+				mappings = {
+					i = {
+						["<C-c>"] = false,
+						["<Esc>"] = require("telescope.actions").close,
+						["<C-n>"] = require("telescope.actions").move_selection_next,
+						["<C-p>"] = require("telescope.actions").move_selection_previous,
+						["<C-j>"] = require("telescope.actions").cycle_history_next,
+						["<C-k>"] = require("telescope.actions").cycle_history_prev,
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden" },
+				},
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		})
+		require("telescope").load_extension("fzf")
+	end,
 }
